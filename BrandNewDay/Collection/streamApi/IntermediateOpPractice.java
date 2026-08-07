cf86c717a2b821b0ae3a72c321fe417de0d71abb
Vikram Laghule <vikramlaghule1@gmail.com>
2026-08-07 23:37:53 +0530

    Add Stream intermediate operations practice

---
+++ BrandNewDay/Collection/streamApi/IntermediateOpPractice.java
@@ -0,0 +1,12 @@
+package streamApi;
+
+import java.util.List;
+
+public class IntermediateOpPractice {
+	public static void main(String[] args)
+	{
+		List<Integer> list=List.of(12,16,15,17,11,19);
+		list.stream()
+		.peek()
+	}
+}
