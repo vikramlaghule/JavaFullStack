a9dc318cf22a29127931034e3ee394541b0899ff
Vikram Laghule <vikramlaghule1@gmail.com>
2026-08-08 11:00:54 +0530

    .

--- BrandNewDay/Collection/streamApi/IntermediateOpPractice.java
+++ BrandNewDay/Collection/streamApi/IntermediateOpPractice.java
@@ -5,8 +5,14 @@ import java.util.List;
 public class IntermediateOpPractice {
 	public static void main(String[] args)
 	{
-		List<Integer> list=List.of(12,16,15,17,11,19);
-		list.stream()
-		.peek()
+		 List<Integer> list = List.of(12, 16, 15, 17, 11, 19);
+
+	        list.stream()
+	            .peek(n -> System.out.println("Before filter: " + n))
+	            .filter(n -> n > 15)
+	            .peek(n -> System.out.println("After filter: " + n))
+	            .forEach(System.out::println);
+	        
+	        
 	}
 }
